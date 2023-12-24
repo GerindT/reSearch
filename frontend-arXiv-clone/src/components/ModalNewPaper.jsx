4a0813681a00ec66b0d06764c5f87d9553a0540a
GerindT <gerindt@gmail.com>
2023-12-24 18:10:42 +0100

    fixes on new paper

--- frontend-arXiv-clone/src/components/ModalNewPaper.jsx
+++ frontend-arXiv-clone/src/components/ModalNewPaper.jsx
@@ -22,10 +22,39 @@ function ModalNewPaper({ openModal, setOpenModal, posts, setPosts }) {
   const handleFileChange = (event) => {
     const file = event.target.files[0];
 
-    setSelectedFile(file);
+    // Check if a file is selected
+    if (file) {
+      // Extract the file extension
+      const parts = file.name.split(".");
+      const extension = parts[parts.length - 1].toLowerCase();
+
+      // Check if the file extension is allowed
+      if (extension === "pdf" || extension === "epub") {
+        setSelectedFile(file);
+        setShowAlert(false); // Reset the alert if it was previously shown
+      } else {
+        // Show alert for invalid file type
+        setShowAlert(true);
+        setSelectedFile(null);
+      }
+    }
   };
 
   const handleSaveChanges = () => {
+    event.preventDefault();
+    // Validation checks
+    if (
+      !title ||
+      title.length < 100 ||
+      !authors ||
+      !abscract ||
+      abscract.length < 200 ||
+      !selectedFile
+    ) {
+      // If any of the required fields is empty, show alert and return
+      setShowAlert(true);
+      return;
+    }
     // All validations passed, proceed with saving changes
     setFile(selectedFile);
     setAuthors(authors);
@@ -35,7 +64,7 @@ function ModalNewPaper({ openModal, setOpenModal, posts, setPosts }) {
       authors: authors,
       abstract: abscract,
       comments: ["comment1", "comment2"],
-      date: Date.now(),
+      date: new Date().toDateString(),
       content:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque id fuga repudiandae ullam aperiam eos maxime aliquam quibusdam soluta explicabo rerum accusantium non, vitae expedita deleniti nesciunt nisi, asperiores nobis quaerat labore? Veniam dolore deleniti maxime? Totam quam minus quasi, eaque, labore iusto quae ullam voluptas architecto perferendis sit similique quidem maxime doloremque officiis dolorum cum asperiores, magni incidunt. Numquam aperiam voluptate ad reiciendis? Voluptatibus veritatis, voluptate quaerat numquam dolores incidunt ullam voluptates, eius debitis consectetur vero nam id quidem, sit magnam? Eveniet laboriosam reprehenderit, suscipit minima voluptas quia quae fugiat, voluptatem similique atque voluptates, itaque eum autem deleniti expedita obcaecati asperiores cum! Dolorum molestias dignissimos, beatae in, alias fuga aliquam quos sed tempora nulla impedit reprehenderit! Mollitia commodi suscipit harum debitis, autem magni assumenda dolores aperiam aspernatur excepturi esse enim dolorum quidem nam illo cupiditate? Autem, impedit. Veritatis commodi iure nisi eaque quam omnis dolor, odit, nemo a quidem impedit doloribus? Error expedita eveniet earum dolores ullam fuga doloribus explicabo, illo amet illum nesciunt voluptatem accusantium corrupti sed eius soluta esse impedit deleniti optio quia blanditiis. Voluptates doloribus quas possimus, nesciunt accusantium laudantium impedit ad aliquam distinctio? Quod ipsa, obcaecati sit magnam optio veritatis ex itaque. Labore vitae, veritatis adipisci deleniti ullam iusto at, quod aspernatur, tempore ipsam ea iste aliquid voluptas eaque. Quidem facilis sit, dolore assumenda, repellendus expedita aliquam doloribus deleniti dolorem cumque quia et alias, commodi explicabo. Eius alias odit necessitatibus eos facere exercitationem ullam! Eos ipsum, nesciunt atque at, quaerat corporis accusantium libero alias, enim modi magnam repellendus. Voluptas maxime, minus modi quos consectetur voluptatibus provident, quam eligendi enim libero eos consequuntur impedit! Saepe soluta repudiandae at sapiente? Beatae deleniti officiis quibusdam aliquam magnam. Minus dolor doloribus cupiditate eum animi, iste delectus nisi, officiis vitae sunt fuga, quidem accusamus in accusantium vero atque maxime corporis excepturi numquam ea recusandae magnam reiciendis suscipit. Itaque ipsum blanditiis, debitis, quod repudiandae architecto commodi omnis nesciunt veniam molestiae totam! Odio, fugiat amet explicabo sapiente dolore corrupti quia ex voluptas beatae, architecto deleniti est numquam, blanditiis aperiam voluptatibus doloribus magnam delectus sint ratione aut error quo obcaecati quod? Dolor ducimus laboriosam quo non minus voluptate vitae, repellat similique magni placeat adipisci quaerat? Eveniet voluptatibus provident laboriosam at porro ex voluptatum, numquam velit ullam magnam? Voluptate nihil provident quisquam tempora, earum dignissimos neque officiis culpa recusandae nisi libero doloremque modi fuga rem magnam qui eum nulla asperiores tenetur quas optio nam. Magni, omnis unde dolorem minus, fugit voluptatum, itaque culpa consectetur quaerat animi dolore explicabo corrupti totam eveniet quisquam nulla! Maiores necessitatibus, amet illo impedit, dolorem velit, odit neque nihil quo tempore pariatur. Dignissimos, veniam? Suscipit quasi minus atque odio itaque. Aliquid ullam incidunt dolorum fuga numquam quo animi accusamus architecto cumque molestias hic voluptatibus culpa illum laborum earum laboriosam eaque, vero dicta sunt itaque neque voluptates vitae dignissimos eligendi! Facilis corporis cum quisquam dignissimos a quis blanditiis natus, assumenda obcaecati, dolorum nostrum accusamus pariatur molestias aperiam reiciendis culpa repudiandae facere, hic voluptate consectetur inventore expedita nam quae eum. Facilis tempore quo voluptatibus beatae? Non accusamus numquam maiores inventore cumque voluptatum. Facilis culpa explicabo accusamus officia nulla ad est eligendi necessitatibus ea recusandae quisquam assumenda veritatis voluptatibus libero, quis ab alias repudiandae autem et ducimus unde soluta officiis reiciendis! Quo tempora a consequuntur, repellat dignissimos quaerat. Inventore exercitationem perspiciatis quas animi voluptas minima asperiores corporis, eos blanditiis officia reprehenderit atque est modi et consequuntur quasi iure dolorem culpa neque ab rem qui debitis, delectus voluptatibus? Recusandae mollitia doloremque earum quis quasi eligendi distinctio tempora architecto pariatur dolore voluptas sint corrupti perferendis, rem placeat! Dignissimos placeat facilis quidem ea delectus cupiditate laudantium!",
       isVerified: false,
@@ -51,6 +80,8 @@ function ModalNewPaper({ openModal, setOpenModal, posts, setPosts }) {
     };
 
     setPosts([newPost, ...posts]);
+
+    console.log("New post added", newPost, selectedFile);
 
     setOpenModal(false);
   };
@@ -119,7 +150,7 @@ function ModalNewPaper({ openModal, setOpenModal, posts, setPosts }) {
             <FileInput
               id="file-upload"
               onChange={handleFileChange}
-              accept="image/*"
+              accept=".epub, .pdf"
               required
             />
           </div>
