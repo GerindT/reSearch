cea885db900e5069eb83e0780b978891bc933b41
GerindT <gerindt@gmail.com>
2023-12-24 20:26:47 +0100

    added coffe

--- frontend-arXiv-clone/src/pages/Home.jsx
+++ frontend-arXiv-clone/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { useOutletContext } from "react-router-dom";
 
 function Home() {
   const [posts, setPosts] = useOutletContext();
+  console.log(posts);
   return (
     <div className="flex flex-col justify-center items-center gap-[2rem] mt-[2em]">
       <Blockquote className="text-center">
@@ -13,21 +14,31 @@ function Home() {
         everything, we ought to know a little about everything.&quot;
       </Blockquote>
       <InfoAccordion />
-      {posts.map((p, index) => (
-        <Posts
-          key={index}
-          title={p.title}
-          author={p.authors}
-          abstract={p.abstract}
-          date={p.date}
-          isVerified={p.isVerified}
-          verDate={p.verDate}
-          isFav={p.isFav}
-          favDate={p.favDate}
-          likes={p.likes}
-          categories={p.categories}
-        />
-      ))}
+      {!posts.length > 0 ? (
+        <div className="h-[70vh] flex flex-col justify-center self-center">
+          <p className="font-thin text-xs text-gray-500 text-center mr-[4em]">
+            There were no results for your query. Try again with different
+            terms.
+          </p>
+          <img src="/giphy.gif" alt="coffe crying" />
+        </div>
+      ) : (
+        posts.map((p, index) => (
+          <Posts
+            key={index}
+            title={p.title}
+            author={p.authors}
+            abstract={p.abstract}
+            date={p.date}
+            isVerified={p.isVerified}
+            verDate={p.verDate}
+            isFav={p.isFav}
+            favDate={p.favDate}
+            likes={p.likes}
+            categories={p.categories}
+          />
+        ))
+      )}
     </div>
   );
 }
