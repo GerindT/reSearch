d667880a2ae7c792a2859a79e4ba94766e965cab
Gerind <52495522+GerindT@users.noreply.github.com>
2024-01-26 16:54:37 +0100

    Update dashboard.php

--- backend-arXiv/api/dashboard.php
+++ backend-arXiv/api/dashboard.php
@@ -10,7 +10,7 @@ session_set_cookie_params([
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-header("Access-Control-Allow-Origin: https://arxiv-frontend.netlify.app");
+header("Access-Control-Allow-Origin: https://re-search.netlify.app");
 header("Access-Control-Allow-Headers: Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT,DELETE");
 header("Access-Control-Allow-Credentials: true");
